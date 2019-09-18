7c7bb2cc8d1ee6a586685bf49a8920a8e095adee
Jason <jasonxu_1990@163.com>
2019-09-18 15:35:48 +0800

    整合代码，添加lottie动画，不是很稳定

---
+++ android/app/src/main/java/com/jason/myfluttertest/wxapi/WXEntryActivity.java
@@ -0,0 +1,13 @@
+package com.jason.myfluttertest.wxapi;
+
+import android.os.Bundle;
+
+import com.jarvan.fluwx.wxapi.FluwxWXEntryActivity;
+
+public class WXEntryActivity extends FluwxWXEntryActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
